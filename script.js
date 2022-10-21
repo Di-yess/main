b1ef06e50db0f618d7eca4bdb11edbe4e1cbd2b6
Dmitriy <116383215+Di-yess@users.noreply.github.com>
2022-10-21 23:10:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function change_to_red(){
+    const color=document.getElementsByClassName('img');
+    color[0].style.backgroundImage='url(imgs/t-red.jpg)';
+}
+
+function change_to_white(){
+    const color=document.getElementsByClassName('img');
+    color[0].style.backgroundImage='url(imgs/t-shirt_check.jpg)';
+}
+
+function change_to_yellow(){
+    const color=document.getElementsByClassName('img');
+    color[0].style.backgroundImage='url(imgs/t-yellow.jpg)';
+}
+
+function change_to_green(){
+    const color=document.getElementsByClassName('img');
+    color[0].style.backgroundImage='url(imgs/t-green.jpg)';
+}
